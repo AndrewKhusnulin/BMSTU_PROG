523a383bf275f801f11be63c88426de93af21c34
Андрей <okdahorosho@gmail.com>
2016-12-09 00:23:44 +0300

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,49 +1,43 @@
 #include <iostream>
- using namespace std;
- double sum(float a, float b){
-     return a + b;
- }
- double sub(float a, float b){
-     return a - b;
+using namespace std;
+int sum(int a, int b) 
+{
+	return a + b;
 }
- double multiply(float a, float b){
-     return a*b;
- }
- double *div(float a, float b){
-    if (b != 0) {
-		double *rez = new double;
-		*rez = a / b;
-		return rez;
-                }
-	 Else return nulptr;
- }	 
- double pow1(float a, int b){
-    float new_a = 1;
-    int i =1;
-    for(i = 1; i <= b; i++){
-        new_a = new_a * a;
-     }
-     return new_a;
- }
- double sqrt1(float a) {
-     double n = 1;
-     int i = 1;
-     for(i;i<a;i++)
-     {
-         if (pow1(n,2) == a) break;
-         else (n = 0.5*(n + a/pow1(n, 2)));
-     }
-     return n;
- }
- int main() {
-     cin >> a >> b;
-     cout << sqrt1(a) << endl;
-     cout << pow1(a, b) <<endl;
-     double *rez = div(a, b);
-	    if (rez) cout << *rez << endl;
-	    else cout << "Cant divide by zero" << endl;
-     cout << multiply(a, b) << endl;
-     cout << sum(a, b) << endl;
-     cout << sub(a, b) << endl;
-     return 0;
- }
+int sub(int a, int b) 
+{
+	return a - b;
+}
+int multiply(int a, int b) 
+{
+		return a*b;	
+}
+int main(int argc, char *argv[]) 
+{
+	setlocale(LC_ALL, "rus");
+	int a, b;
+	if (argc < 3)
+	{
+		printf_s("Ошибка, параметров в командной строке не хватает для задания исходных данных.\n");
+		system("pause");
+		return 1;
+	}
+	if (!sscanf_s(argv[1], "%d", &a))
+
+	{
+		cout << "Ошибка, неверный формат первого входного параметра.\n" << endl;
+		system("pause");
+		return 1;
+	}
+	if (!sscanf_s(argv[2], "%d", &b))
+	{
+		cout << "Ошибка, неверный формат второго входного параметра.\n" << endl;
+		system("pause");
+		return 1;
+	}
+	cout << "Сумма: " << sum(a, b) << endl;
+	cout << "Разность :" << sub(a, b) << endl;
+	cout << "Произведение :" << multiply(a, b) << endl;
+	system("pause");
+	return 0;
+}
